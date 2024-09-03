4237a811861608b092887cfa5759d115d2792a4e
Mohamed Gehad <mogehad2030@gmail.com>
2024-09-03 20:08:47 +0300

    Added fonts and texts style

---
+++ lib/core/styles/texts_style.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+
+abstract class TextsStyle {
+  static const TextStyle textStyleRegular9 = TextStyle(
+    fontSize: 9,
+    fontFamily: 'Poppins',
+    fontWeight: FontWeight.w400,
+  );
+  static const TextStyle textStyleRegular11 = TextStyle(
+    fontSize: 11,
+    fontFamily: 'Poppins',
+    fontWeight: FontWeight.w400,
+  );
+  static const TextStyle textStyleRegular12 = TextStyle(
+    fontSize: 12,
+    fontFamily: 'Poppins',
+    fontWeight: FontWeight.w400,
+  );
+
+  static const TextStyle textStyleRegular13 = TextStyle(
+    fontSize: 13,
+    fontFamily: 'Poppins',
+    fontWeight: FontWeight.w400,
+  );
+  static const TextStyle textStyleRegular14 = TextStyle(
+    fontSize: 14,
+    fontFamily: 'Poppins',
+    fontWeight: FontWeight.w400,
+  );
+  static const TextStyle textStyleRegular15 = TextStyle(
+    fontSize: 15,
+    fontFamily: 'Poppins',
+    fontWeight: FontWeight.w400,
+  );
+  static const TextStyle textStyleRegular18 = TextStyle(
+    fontSize: 18,
+    fontFamily: 'Poppins',
+    fontWeight: FontWeight.w400,
+  );
+  static const TextStyle textStyleRegular24 = TextStyle(
+    fontSize: 24,
+    fontFamily: 'Poppins',
+    fontWeight: FontWeight.w400,
+  );
+
+  static const TextStyle textStyleMedium14 = TextStyle(
+    fontSize: 14,
+    fontFamily: 'Poppins',
+    fontWeight: FontWeight.w500,
+  );
+  static const TextStyle textStyleMedium16 = TextStyle(
+    fontSize: 16,
+    fontFamily: 'Poppins',
+    fontWeight: FontWeight.w500,
+  );
+  static const TextStyle textStyleMedium17 = TextStyle(
+    fontSize: 17,
+    fontFamily: 'Poppins',
+    fontWeight: FontWeight.w500,
+  );
+  static const TextStyle textStyleMedium18 = TextStyle(
+    fontSize: 18,
+    fontFamily: 'Poppins',
+    fontWeight: FontWeight.w500,
+  );
+  static const TextStyle textStyleMedium26 = TextStyle(
+    fontSize: 26,
+    fontFamily: 'Poppins',
+    fontWeight: FontWeight.w500,
+  );
+  static const TextStyle textStyleMedium32 = TextStyle(
+    fontSize: 32,
+    fontFamily: 'Poppins',
+    fontWeight: FontWeight.w500,
+  );
+
+  static const TextStyle textStyleSemiBold16 = TextStyle(
+    fontSize: 16,
+    fontFamily: 'Poppins',
+    fontWeight: FontWeight.w600,
+  );
+  static const TextStyle textStyleSemiBold24 = TextStyle(
+    fontSize: 24,
+    fontFamily: 'Poppins',
+    fontWeight: FontWeight.w600,
+  );
+  static const TextStyle textStyleSemiBold26 = TextStyle(
+    fontSize: 26,
+    fontFamily: 'Poppins',
+    fontWeight: FontWeight.w600,
+  );
+}
