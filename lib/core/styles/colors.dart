dd392df4ce00495fcc7e34c483a34e7d27cb59a5
Mohamed Gehad <mogehad2030@gmail.com>
2024-09-03 20:31:48 +0300

    Added colors

---
+++ lib/core/styles/colors.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+abstract class AppColors {
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color black = Color(0xFF1E1E2D);
+  static const Color greyA7 = Color(0xFFA2A2A7);
+  static const Color greyF4 = Color(0xFFF4F4F4);
+  static const Color blue = Color(0xFF0066FF);
+  static const Color grey8D = Color(0xFF7E848D);
+  static const Color grey94 = Color(0xFF8B8B94);
+  static const Color grey533 = Color(0xFF232533);
+  static const Color dark = Color(0xFF161622);
+  static const Color grey54 = Color(0xFF4C4C54);
+  static const Color greyC1 = Color(0xFFBBBBC1);
+  static const Color greySE = Color(0xFF41455E);
+  static const Color red = Color(0xFFFF3F60);
+  static const Color grey433 = Color(0xFF242433);
+  static const Color greyB7 = Color(0xFFAAAFB7);
+  static const Color grey37 = Color(0xFF292937);
+}
