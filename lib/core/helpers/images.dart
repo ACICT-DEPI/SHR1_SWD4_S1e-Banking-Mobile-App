2c40a9208895920970bde845b8655f19e41f1406
Mohamed Gehad <mogehad2030@gmail.com>
2024-09-04 11:21:55 +0300

    Added images class

---
+++ lib/core/helpers/images.dart
@@ -0,0 +1,17 @@
+abstract class Images {
+  static const String fontsPoppinsMedium = 'assets/fonts/Poppins-Medium.ttf';
+  static const String fontsPoppinsRegular = 'assets/fonts/Poppins-Regular.ttf';
+  static const String fontsPoppinsSemiBold =
+      'assets/fonts/Poppins-SemiBold.ttf';
+  static const String imagesChip1 = 'assets/images/chip_1.svg';
+  static const String imagesChip2 = 'assets/images/chip_2.svg';
+  static const String imagesEllipse1 = 'assets/images/Ellipse_1.png';
+  static const String imagesEllipse2 = 'assets/images/Ellipse_2.png';
+  static const String imagesLogo = 'assets/images/Logo.svg';
+  static const String imagesMasterCard = 'assets/images/master_card.svg';
+  static const String imagesNfc = 'assets/images/nfc.svg';
+  static const String imagesOnboarding1 = 'assets/images/Onboarding_1.svg';
+  static const String imagesOnboarding2 = 'assets/images/Onboarding_2.svg';
+  static const String imagesOnboarding3 = 'assets/images/Onboarding_3.svg';
+  static const String imagesWorldMap = 'assets/images/worldmap.png';
+}
