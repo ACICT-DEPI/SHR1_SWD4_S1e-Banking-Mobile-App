e6062e4c12e61eabd019c010e845f502af24479d
AhmedSalah-26 <ahmed01142043116@gmail.com>
2024-09-09 16:45:28 +0300

    no message

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
+import 'package:bank_app/features/authentication/presentation/views/login_view.dart';
+import 'package:bank_app/features/authentication/presentation/views/signup_view.dart';
 import 'package:flutter/material.dart';
 
 import 'core/styles/theme_style.dart';
+import 'features/home/presentation/views/home_view.dart';
+import 'features/home/presentation/views/widgets/home_view_body.dart';
 import 'features/onboardingScreen/ui/OnboardingScreen.dart';
+import 'features/statistics/presentation/views/statistics_view.dart';
 import 'features/transaction_history/presentation/views/transaction_history.dart';
+import 'features/transaction_history/presentation/views/widgets/transaction_history_body.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,7 +22,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeStyle.lightThemeData,
-      home:  OnboardingScreen(),
+      home:  StatisticsView(),
     );
   }
 }
